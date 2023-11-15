a424456f99a1e8868c8baf925997df873beac1bd
DESKTOP-V1IS21O\Puesto8 <frandanigd@gmail.com>
2023-11-15 12:58:10 +0100

    19/10/2023

--- ScaffoldingDIW/Program.cs
+++ ScaffoldingDIW/Program.cs
@@ -25,3 +25,29 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+//using ScaffoldingDIW.Models;
+
+//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        using (var db = new GestorBibliotecaPersonalContext())
+//        {
+//            // Creating a new item and saving it to the database
+//            var newItem = new Prueba();
+//            newItem.Nombre = "Francisco Jose";
+//            newItem.Apellidos = "Gallego Dorado";
+//            db.Pruebas.Add(newItem);
+//            var count = db.SaveChanges();
+//            Console.WriteLine("{0} records saved to database", count);
+//            // Retrieving and displaying data
+//            Console.WriteLine();
+//            Console.WriteLine("All items in the database:");
+//            foreach (var item in db.Pruebas)
+//            {
+//                Console.WriteLine("{0} | {1}", item.Nombre, item.Apellidos);
+//            }
+//        }
+//    }
+//}
